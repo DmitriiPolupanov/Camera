9002f5e186af8301aec32cac59b5dcc4dc721c25
Dmitrii Polupanov <demetrious@mail.ru>
2019-11-28 20:39:33 +0500

    Cлучайные окружности

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import sys
-from PyQt5 import uic
 from random import randint
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPainter, QColor
+from Ui import Ui_Form
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi("Ui.ui", self)
+        self.setupUi(self)
 
         self.flag = False
         self.pushButton.clicked.connect(self.clicked)
@@ -16,15 +16,15 @@ class MyWidget(QMainWindow):
     def paintEvent(self, event):
         qp = QPainter()
         qp.begin(self)
-        qp.setBrush(QColor(255, 255, 0))
         self.draw(qp)
         qp.end()
 
     def draw(self, qp):
         if self.flag:
             for i in range(randint(1, 100)):
-                self.x = randint(0, 450)
-                self.y = randint(0, 450)
+                qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
+                self.x = randint(0, 900)
+                self.y = randint(0, 500)
                 self.r = randint(10, 60)
                 qp.drawEllipse(self.x, self.y, self.r, self.r)
 
